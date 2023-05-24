788729a652c19a8b917bdd1141ffc77d31c04ed8
Kunal Kawate <114325267+Kunal-kawate@users.noreply.github.com>
2023-05-24 15:56:27 +0530

    Update main.py by me kunal
    project is done

--- main.py
+++ main.py
@@ -4,28 +4,41 @@ import numpy as np
 import csv
 import os
 from datetime import datetime
- 
+#  ----------------------------------------whats app code-----------------------
+
+import pyautogui as pg
+import webbrowser as web
+import time
+import pandas as pd
+import pywhatkit
+# -----------------------------------end------------------------------
 video_capture = cv2.VideoCapture(0)
- 
-jobs_image = face_recognition.load_image_file("photos/durgesh.jpg")
+
+no_list={"Kunal kawate":"91_8767563012","Kunal Paithane":"91_8767563012","shreyas":"91_8788654516","Ritesh":"91_9325721243"}
+jobs_image = face_recognition.load_image_file("photos/kunya.jpg")
 jobs_encoding = face_recognition.face_encodings(jobs_image)[0]
  
-ratan_tata_image = face_recognition.load_image_file("photos/prajakta.jpg")
+ratan_tata_image = face_recognition.load_image_file("photos/kunalp.jpg")
 ratan_tata_encoding = face_recognition.face_encodings(ratan_tata_image)[0]
  
-sadmona_image = face_recognition.load_image_file("photos/ketan.jpg")
+sadmona_image = face_recognition.load_image_file("photos/rcbsamrthak.jpg")
 sadmona_encoding = face_recognition.face_encodings(sadmona_image)[0]
+
+s_image = face_recognition.load_image_file("photos/shreyas.jpg")
+s_encoding = face_recognition.face_encodings(s_image)[0]
  
 known_face_encoding = [
 jobs_encoding,
 ratan_tata_encoding,
-sadmona_encoding
+sadmona_encoding,
+s_encoding
 ]
  
 known_faces_names = [
-"durgesh",
-"prajakta",
-"ketan"
+"Kunal kawate",
+"Kunal Paithane",
+"Ritesh",
+"shreyas"
 ]
  
 students = known_faces_names.copy()
@@ -43,6 +56,7 @@ current_date = now.strftime("%Y-%m-%d")
  
 f = open(current_date+'.csv','w+',newline = '')
 lnwriter = csv.writer(f)
+lnwriter.writerow(["LeadNumber","Message"])
  
 while True:
     _,frame = video_capture.read()
@@ -79,9 +93,10 @@ while True:
  
                 if name in students:
                     students.remove(name)
+                    name1=no_list[name]
                     print(students)
                     current_time = now.strftime("%H-%M-%S")
-                    lnwriter.writerow([name,current_time])
+                    lnwriter.writerow([name1,current_time])
     cv2.imshow("attendence system",frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -89,3 +104,26 @@ while True:
 video_capture.release()
 cv2.destroyAllWindows()
 f.close()
+
+# ------------------------------whats code -----------------------------
+
+data = pd.read_csv("2023-05-24.csv")
+data_dict = data.to_dict('list')
+leads = data_dict['LeadNumber']
+messages = data_dict['Message']
+combo = zip(leads,messages)
+first = True
+for lead,message in combo:
+    time.sleep(4)
+    pywhatkit.sendwhatmsg_instantly(lead, message,10, tab_close=True)
+    if first:
+        time.sleep(6)
+        first=False
+    width,height = pg.size()
+    pg.click(width/2,height/2)
+    time.sleep(8)
+    pg.press('enter')
+    time.sleep(8)
+    pg.hotkey('ctrl', 'w')
+
+# --------------------end code----------------------------------------------------------
